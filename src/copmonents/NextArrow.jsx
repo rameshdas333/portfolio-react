5dd38ad34ccff39904e7645bac9ed102ad5fdd39
rameshdas333 <rameshdascse@gmail.com>
2025-09-08 00:24:51 +0600

    last commit data

--- src/copmonents/NextArrow.jsx
+++ src/copmonents/NextArrow.jsx
@@ -7,13 +7,13 @@ const NextArrows = ({onClick}) => {
     return (
         <div>
           
-      <Container>
+   
       <div
     className="w-10 h-10 hidden md:flex items-center justify-center z-50 absolute -right-4 top-1/2 bg-white border rounded-full shadow hover:bg-red-500 hover:text-white cursor-pointer p-2"
     onClick={onClick}>
     <FaArrowRight size={20} />
   </div>
-      </Container>
+    
 
         </div>
     );
--- src/copmonents/PrevArrow.jsx
+++ src/copmonents/PrevArrow.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
-import Container from './Container';
 
 const PrevArrows = ({onClick}) => {
     return (
         <div>
            
-  <Container>
+  
 {/* absolute z-10 -top-11 right-14 transform -translate-y-1/2 */}
     <div
     className="w-10 h-10 hidden md:flex z-50 absolute top-1/2 -left-4 items-center justify-center bg-white border rounded-full shadow hover:bg-red-500 hover:text-white cursor-pointer p-2"
@@ -14,7 +12,7 @@ const PrevArrows = ({onClick}) => {
   >
     <FaArrowLeft size={20} />
   </div>
-  </Container>
+ 
 
         </div>
     );
